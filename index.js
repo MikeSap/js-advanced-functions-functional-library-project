5a76a01f812f2cfc167504a66697a662adbeff2e
laurencun <lauren.busbee@gmail.com>
2020-11-04 14:00:21 -0600

    started

--- index.js
+++ index.js
@@ -4,12 +4,27 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, func) {
+      let newCollection;      
+      if (typeof collection === 'object'){
+            newCollection = Object.values(collection)
+            }else{
+            newCollection = collection
+            }
+            newCollection.forEach(item => {
+              func(item)
+            })
+           return collection
+  },
 
-    },
-
-    map: function() {
-
+    map: function(collection, func) {  
+      if (collection instanceof Object){
+        debugger
+            let collection = Object.values(collection)
+            }
+            return collection.map(item => {
+              func(item)
+            })
     },
 
     reduce: function() {
